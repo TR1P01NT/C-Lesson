b316d15ee20a4c0b1e87ff4397b26a14fd66cddf
Kev Lee <kevinleect002@gmail.com>
2021-11-07 18:42:00 +0800

    Week 1

---
+++ C/Week 1/five_digit.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+    int num, num1, num2, num3, num4, num5;
+
+    scanf("%d", &num);
+
+    num1 = num % 10;
+    num /= 10;
+    num2 = num % 10;
+    num /= 10;
+    num3 = num % 10;
+    num /= 10;
+    num4 = num % 10;
+    num /= 10;
+    num5 = num % 10;
+
+    printf("%d   %d   %d   %d   %d", num1, num2, num3, num4, num5);
+}
