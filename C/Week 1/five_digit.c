3077e1de2d99be7444b8b87228dc996bfc85fee5
Kev Lee <kevinleect002@gmail.com>
2021-11-07 18:44:24 +0800

    Week 1 Correction

--- C/Week 1/five_digit.c
+++ C/Week 1/five_digit.c
@@ -5,15 +5,15 @@ int main(void) {
 
     scanf("%d", &num);
 
-    num1 = num % 10;
-    num /= 10;
-    num2 = num % 10;
-    num /= 10;
-    num3 = num % 10;
+    num5 = num % 10;
     num /= 10;
     num4 = num % 10;
     num /= 10;
-    num5 = num % 10;
+    num3 = num % 10;
+    num /= 10;
+    num2 = num % 10;
+    num /= 10;
+    num1 = num % 10;
 
     printf("%d   %d   %d   %d   %d", num1, num2, num3, num4, num5);
 }
