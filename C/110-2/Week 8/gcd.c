30559ebb432691e57b4cb50608685209df14f687
Kev Lee <kevinleect002@gmail.com>
2022-04-20 11:35:30 +0800

    Week 8

---
+++ C/110-2/Week 8/gcd.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int gcd(int x, int y) {
+    if (y != 0) {
+        return gcd(y, x % y);
+    }
+    else {
+        return x;
+    }
+}
+
+int main(void) {
+    int n, i, j;
+    int G = 0;
+
+    scanf("%d", &n);
+
+    while (1) {
+        if (n == 0) {
+            break;
+        }
+
+        for (i = 1; i < n; i++) {
+            for (j = i + 1; j <= n; j++) {
+                G += gcd(i, j);
+            }
+        }
+
+        printf("%d\n", G);
+        G = 0;
+
+        scanf("%d", &n);
+    }
+}
