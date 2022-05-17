09b440f60b9158bce3a8826cded3854e6002e10d
Kev Lee <kevinleect002@gmail.com>
2022-05-17 12:17:27 +0800

    Week 12

--- C/110-2/Week 12/binarytree.c
+++ C/110-2/Week 12/binarytree.c
@@ -8,16 +8,25 @@ struct node
     struct node *right;
 };
 
-int flag = 0;
+int past[100];
 
 void inorderTraversal(struct node *root);
-struct node *insert(struct node *node, int val);
+struct node *insertnode(struct node *node, int val);
+struct node *createnode(int val);
+// int searchnode(struct node *root, int val);
+int checknum(int val);
 
 int main(void)
 {
     struct node *root = NULL;
     int choice;
     int num;
+    int flag = 0;
+    int i = 0;
+
+    for (int x = 0; x < 100; x++) {
+        past[x] = -99;
+    }
 
     scanf("%d", &choice);
 
@@ -27,14 +36,23 @@ int main(void)
         {
         case 1:
             scanf("%d", &num);
-            root = insert(root, num);
+            root = insertnode(root, num);
+            past[i++] = num;
             break;
         case 2:
             scanf("%d", &num);
-            root = insert(root, num);
+            root = insertnode(root, num);
+            past[i++] = num;
             break;
         case 3:
             scanf("%d", &num);
+            flag = checknum(num);
+            if (flag) {
+                printf("Yes\n");
+            }
+            else {
+                printf("No\n");
+            }
             break;
         case 4:
             inorderTraversal(root);
@@ -46,8 +64,7 @@ int main(void)
     }
 }
 
-void inorderTraversal(struct node *root)
-{
+void inorderTraversal(struct node *root) {
     if (root != NULL) {
         inorderTraversal(root->left);
         printf("%d ", root->data);
@@ -58,21 +75,54 @@ void inorderTraversal(struct node *root)
 struct node *createnode(int val) {
     struct node *temp = (struct node *)malloc(sizeof(struct node));
     temp -> data = val;
-    temp -> left = temp -> right = NULL;
+    temp -> left = NULL;
+    temp -> right = NULL;
     return temp;
 }
 
-struct node *insert(struct node *node, int val) {
+struct node *insertnode(struct node *node, int val) {
     if (node == NULL) {
         return createnode(val);
     }
 
-    if (val < node->data) {
-        node -> right = insert(node->right, val);
-    }
-    else {
-        node -> left = insert(node->left, val);
+    int flag = 0;
+    flag = checknum(val);
+
+    if (!flag) {
+        if (val < node->data) {
+        node -> right = insertnode(node->right, val);
+        }
+        else {
+            node -> left = insertnode(node->left, val);
+        }
     }
 
     return node;
+}
+
+// int searchnode(struct node *root, int val) {
+//     if (root == NULL) {
+//         return 0;
+//     }
+
+//     if (root->data == val) {
+//         return 1;
+//     }
+
+//     if (val < root->data) {
+//         return searchnode(root->right, val);
+//     }
+//     else {
+//         return searchnode(root->left, val);
+//     }
+// }
+
+int checknum(int val) {
+    int i;
+    for (i = 0; i < 100; i++) {
+        if (past[i] == val) {
+            return 1;
+        }
+    }
+    return 0;
 }
