1073250b74cd9538ae76dc144f7914df7a85f369
Kev Lee <kevinleect002@gmail.com>
2022-10-20 17:57:13 +0800

    CMake removal

--- 111/Data Structure/Queue/cmake-build-debug/cmake_install.cmake
+++ 111/Data Structure/Queue/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/kevlee/Documents/School/DataStruct/Queue
+# Install script for directory: /Users/kevlee/Documents/GitHub/C Lesson/111/Data Structure/Queue
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/kevlee/Documents/School/DataStruct/Queue/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/kevlee/Documents/GitHub/C Lesson/111/Data Structure/Queue/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
